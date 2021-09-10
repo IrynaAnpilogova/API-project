371675785cdf865fa8cdb77d1dac38f9344e1045
Iryna Anpilogova <io.lukash@gmail.com>
2021-09-10 13:00:25 -0700

    add hooks

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -4,30 +4,48 @@ import { expect } from 'chai';
 
 describe('auth', function() {
     const request = supertest(process.env.BASE_URL);
+    let result;
 
-    it('successful log in', function() {
-        request
-            .post('/auth')
-            .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-            .end(function(err, res) {
-                expect(res.statusCode).to.eq(200);
-                expect(res.body.token).not.to.be.undefined;
+    describe('succesful log in', function() {
+        before(function() {
+            result = supertest(process.env.BASE_URL)
+                .post('/auth')
+                .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
+        });
+
+        it('responce status code is 200', function() {
+            result.expect(200);
+        });
+
+        it('responce body contains authorization token', function() {
+            result.end(function(err, res) {
+                expect(res.body.token).not.to.be.undefined
             });
+        });
 
     });
 
+    describe('log in with wrong credentials should return error', function() {
 
-    it('login with wrong credentials should return error', function() {
-        request
-            .post('/auth')
-            .send({ login: 'wrong', password: 'wrong' })
-            .end(function(err, res) {
-                expect(res.statusCode).to.eq(404);
-                expect(res.body.token).not.to.be.eq('Wrong login or passworrd');
+        before(function() {
+            result = supertest(process.env.BASE_URL)
+                .post('/auth')
+                .send({
+                    login: 'wrong',
+                    password: 'wrong'
+                })
+        });
+
+        it('responce status code is 404', function() {
+            result.expect(404);
+        });
+
+        it('responce body contains error message', function() {
+            result.end(function(err, res) {
+                expect(res.body.message).to.eq('Wrong login or password')
+
             });
-
+        });
     });
-
-
 
 });
